6f06292ef48bffc458bbf6155bd036b16a8a0248
Benjamin Boucher-Charest <47901054+BenjaminB-Charest@users.noreply.github.com>
2024-02-28 14:09:36 -0500

    Upgrading regex to detect all methods

--- sample.java
+++ sample.java
@@ -1,18 +1,25 @@
 public class ArrayDemo {
+
 public static void main(String[] args) {
-	int[] anArray;	        // DECLARE an array of integers
+        int[] anArray;	       
 
-	anArray = new int[10];	// CREATE an array of integers
+        anArray = new int[10];	
 
-	// assign a value to each array element 
-	for (int i = 0; i < anArray.length; i++) {
-		anArray[i] = i;
-	}
 
-	// print a value from each array element
-	for (int j = 0; j < anArray.length; j++) {
-	    System.out.print(anArray[j] + " ");
-	}
-	System.out.println();
-    }
+        for (int i = 0; i < anArray.length; i++) {
+            anArray[i] = i;
+        }
+    
+        for (int j = 0; j < anArray.length; j++) {
+            System.out.print(anArray[j] + " ");
+        }
+        System.out.println();
+}
+    
+
+public int sum(int a, int b){
+        return a + b;
+}
+    
+    
 }
